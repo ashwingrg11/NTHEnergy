a2bcd564cfe47553155d4c8c9e34dc1624078583
Ashwin Gurung <ashwin.gurung@cloudyfox.com>
2021-12-14 13:59:37 +0000

    dashboard page designed.

--- src/main/java/application/nthenergy/controllers/DashboardController.java
+++ src/main/java/application/nthenergy/controllers/DashboardController.java
@@ -1,14 +1,37 @@
 package application.nthenergy.controllers;
 
+import application.nthenergy.Dashboard;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private HBox menuLogout;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    void logoutMenuClicked(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Dashboard.class.getResource("views/login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("NTH Energy - Customer Data Platform - Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
