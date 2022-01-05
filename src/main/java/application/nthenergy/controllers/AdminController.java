3ed1f1138e1c4625b9dcccb6ab4a0475435bb73a
Ashwin Gurung <ashwin.gurung@cloudyfox.com>
2022-01-05 23:27:44 +0000

    admin pages design

--- src/main/java/application/nthenergy/controllers/AdminController.java
+++ src/main/java/application/nthenergy/controllers/AdminController.java
@@ -7,6 +7,7 @@
 package application.nthenergy.controllers;
 
 import application.nthenergy.Dashboard;
+import application.nthenergy.core.Helper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +15,90 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class AdminController {
+    @FXML
+    private Button cancelNewTariffBtn;
+
+    @FXML
+    private Label formMsg;
+
+    @FXML
+    private TextField newAdminEmail;
+
+    @FXML
+    private TextField newAdminFirstName;
+
+    @FXML
+    private TextField newAdminLastName;
+
+    @FXML
+    private TextField newAdminMiddleName;
+
+    @FXML
+    private TextField newAdminMobNo;
+
+    @FXML
+    private TextField newAdminPassword;
+
+    @FXML
+    private TextField newAdminUsername;
+
+    @FXML
+    private Button submitNewAdminBtn;
+
+    @FXML
+    private Button viewAllAdminsBtn;
+
+    @FXML
+    private Button addAdminBtn;
+
+    @FXML
+    private Button deleteAdminBtn;
+
+    @FXML
+    private Button editAdminBtn;
+
+
+    @FXML
+    void onClickAddAdmin(MouseEvent event) throws IOException {
+        Helper.setScene(event, "views/admins/new-admin.fxml");
+    }
+
+    @FXML
+    void onClickViewAllAdmins(MouseEvent event) throws IOException {
+        Helper.setScene(event, "views/admins/all-admins.fxml");
+    }
+
+    @FXML
+    void onTextChanged(KeyEvent event) {
+
+    }
+
+    @FXML
+    void onClickSubmitNewAdmin(MouseEvent event) {
+        TextField[] fields = {newAdminFirstName, newAdminLastName, newAdminEmail, newAdminPassword, newAdminMobNo};
+        if(Helper.validateRequiredTextFields(submitNewAdminBtn, fields)) {
+            if (Helper.emailValidate(newAdminEmail, submitNewAdminBtn)) {
+                formMsg.getStyleClass().removeAll("red-text");
+                formMsg.getStyleClass().add("green-text");
+                formMsg.setText("-  New admin has been added successfully.");
+            }
+        }
+    }
+
+    /**
+     * This method gets executed before loading the view.
+     * @return void
+     */
+    public void initialize() {
+        if (editAdminBtn != null) editAdminBtn.setDisable(true);
+        if (deleteAdminBtn != null) deleteAdminBtn.setDisable(true);
+    }
 }
